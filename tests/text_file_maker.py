0f31fb8238e15465b60b6562f2fc389a467b6bf0
Kirill Petrov <kkpetrov0@gmail.com>
2026-09-02 12:06:19 +0300

    Move tests out of task folders

---
+++ tests/text_file_maker.py
@@ -0,0 +1,41 @@
+import csv
+import random
+
+def make_file(path):
+    with open(path, "w", newline="") as file:
+        gentext = csv.writer(file, delimiter=";")
+        gentext.writerow(["timestamp","sensor_id","value"])
+        n = 0
+        for i in range(100_000):
+            if i % 5000 == 0:
+                if n == 0:
+                    gentext.writerow([random.uniform(0, 1000),random.randint(0,50),""])
+                    n += 1
+                elif n == 1:
+                    gentext.writerow(["akdlsda",random.randint(0,50),random.uniform(15,45)])
+                    n += 1
+                elif n == 2:
+                    gentext.writerow([random.uniform(0, 1000),random.randint(0,50),random.uniform(15,45), random.uniform(0, 1000)])
+                    n = 0
+            gentext.writerow([random.uniform(0, 1000),random.randint(0,50),random.uniform(15,45)])
+    return path
+
+'''
+with open("task-05-sensorstats/text_file.csv", "w", newline="") as file:
+    gentext = csv.writer(file, delimiter=";")
+    gentext.writerow(["timestamp","sensor_id","value"])
+    n = 0
+    for i in range(100_000):
+        if i % 5000 == 0:
+            if n == 0:
+                gentext.writerow([random.uniform(0, 1000),random.randint(0,50),""])
+                n += 1
+            elif n == 1:
+                gentext.writerow(["akdlsda",random.randint(0,50),random.uniform(15,45)])
+                n += 1
+            elif n == 2:
+                gentext.writerow([random.uniform(0, 1000),random.randint(0,50),random.uniform(15,45), random.uniform(0, 1000)])
+                n = 0
+        gentext.writerow([random.uniform(0, 1000),random.randint(0,50),random.uniform(15,45)])
+
+'''
